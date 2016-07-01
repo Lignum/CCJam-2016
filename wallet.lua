701552bebda786539d65b875344ddf5059134ca2
Lignum <grgmckinlay@gmail.com>
2016-07-01 19:27:17 +0200

    Login screen

--- wallet.lua
+++ wallet.lua
@@ -39,8 +39,7 @@ local function require(file)
 	end
 end
 
-
-local addressInUse = "kre3w0i79j"
+local addressInUse = ""
 
 local krist = require("src/krist.lua")
 local sheets = require("src/sheets.lua")
@@ -48,12 +47,59 @@ local sheets = require("src/sheets.lua")
 local app = sheets.Application()
 local screen = app.screen
 
+local introContainer = screen + sheets.Container(0, 0, screen.width, screen.height)
+introContainer.style:setField("colour", sheets.colour.lightBlue)
+
+local inputDataBox = introContainer + sheets.Container(2, math.floor(introContainer.height / 2) - 3, introContainer.width - 4, 6)
+inputDataBox.style:setField("colour", sheets.colour.cyan)
+
+local startTxt = "Start \16"
+local startButton = inputDataBox + sheets.Button(inputDataBox.width - #startTxt - 3, inputDataBox.height - 2, #startTxt + 2, 1, startTxt)
+startButton.style:setField("colour", sheets.colour.grey)
+startButton.style:setField("colour.pressed", sheets.colour.grey)
+
+local privatekeyTxt = "Password: "
+local privatekeyText = inputDataBox + sheets.Text(1, 1, #privatekeyTxt, 1, privatekeyTxt)
+privatekeyText.style:setField("colour", sheets.colour.cyan)
+privatekeyText.style:setField("textColour", sheets.colour.black)
+local privatekeyBox = inputDataBox + sheets.TextInput(privatekeyText.width + 1, 1, inputDataBox.width - privatekeyText.width - 2, 1)
+privatekeyBox.style:setField("mask", "*")
+
+local addressText = inputDataBox + sheets.Text(1, 3, inputDataBox.width - 1, 1, "=> ...")
+addressText.style:setField("colour", sheets.colour.cyan)
+
+function privatekeyBox:onUnFocus()
+	local address = krist.makeAddress(privatekeyBox.text)
+
+	if #privatekeyBox.text > 0 and address then
+		addressText:setText("=> " .. address)
+		addressText.style:setField("textColour", sheets.colour.black)
+
+		startButton.style:setField("colour", sheets.colour.green)
+		startButton.style:setField("colour.pressed", sheets.colour.lime)
+
+		startButton.validAddress = true
+	else
+		addressText:setText("=> N/A")
+		addressText.style:setField("textColour", sheets.colour.red)
+
+		startButton.style:setField("colour", sheets.colour.grey)
+		startButton.style:setField("colour.pressed", sheets.colour.grey)
+
+		startButton.validAddress = false
+	end
+end
+
+local privatekey = ""
+
+local walletContainer = screen + sheets.Container(screen.width, 0, screen.width, screen.height)
+
 local sidebarRelativeWidth = 0.32
 local sidebarWidth = math.floor(screen.width * sidebarRelativeWidth + 0.5)
 local infoContainer = nil
 
 -- Sidebar creation
-local sidebar = screen + sheets.Container(0, 0, sidebarWidth, screen.height)
+local sidebar = walletContainer + sheets.Container(0, 0, sidebarWidth, screen.height)
 sidebar.style:setField("colour", sheets.colour.lightBlue)
 
 local function setGKWStyle(comp)
@@ -132,6 +178,9 @@ local function createTransactionPanel()
 	makeTransferText.style:setField("colour", sheets.colour.lightGrey)
 	makeTransferText.style:setField("textColour", sheets.colour.black)
 
+	--makeTransferButton.style:setField("colour", sheets.colour.green)
+	--makeTransferButton.style:setField("colour.pressed", sheets.colour.lime)
+
 	local toTxt = "Recipient: "
 	local toText = makeTransferPanel + sheets.Text(1, 3, #toTxt, 1, toTxt)
 	toText.style:setField("colour", sheets.colour.lightGrey)
@@ -150,25 +199,74 @@ local function createTransactionPanel()
 	amtField.style:setField("textColour", sheets.colour.lightGrey)
 	amtField.style:setField("textColour.focussed", sheets.colour.white)
 
-	function amtField:onUnFocus()
+	local makeTransferButtonText = "Transfer \16"
+	local makeTransferButton = makeTransferPanel + sheets.Button(makeTransferPanel.width - 3 - #makeTransferButtonText, makeTransferPanel.height - 2, #makeTransferButtonText + 2, 1, makeTransferButtonText)
+
+	local stateText = makeTransferPanel + sheets.Text(1, 7, makeTransferPanel.width - 3 - makeTransferButton.width, 1, "Idle")
+	stateText.style:setField("colour", sheets.colour.lightGrey)
+	stateText.style:setField("textColour", sheets.colour.grey)
+
+	local function confirmTransferButtonState()
+		local valid = true
+
 		if tonumber(amtField.text) == nil then
+			valid = false
 			amtField.style:setField("colour", sheets.colour.red)
 		else
 			amtField.style:setField("colour", sheets.colour.grey)
 		end
+
+		if #toField.text ~= 10 then
+			valid = false
+			toField.style:setField("colour", sheets.colour.red)
+		else
+			toField.style:setField("colour", sheets.colour.grey)
+		end
+
+		--makeTransferButton.style:setField("colour", valid and sheets.colour.green or sheets.colour.grey)
+		--makeTransferButton.style:setField("colour.pressed", valid and sheets.colour.lime or sheets.colour.grey)
+		return valid
 	end
 
-	local makeTransferButtonText = "Transfer \16"
-	local makeTransferButton = makeTransferPanel + sheets.Button(makeTransferPanel.width - #makeTransferButtonText - 3, 7, #makeTransferButtonText + 2, 1, makeTransferButtonText)
-	makeTransferButton.style:setField("colour", sheets.colour.green)
-	makeTransferButton.style:setField("colour.pressed", sheets.colour.lime)
+	function makeTransferButton:onClick(btn)
+		--[[if btn == 1 and confirmTransferButtonState() then
+			local amount = tonumber(amtField.text)
+			local toAddr = toField.text
+
+			if krist.performTX(privatekey, toAddr, amount) == nil then
+				stateText:setText("Failed")
+				stateText.style:setField("colour", sheets.colour.red)
+			else
+				stateText:setText("Transaction complete")
+				stateText.style:setField("colour", sheets.colour.lime)
+			end
+		end]]
+	end
+
+	confirmTransferButtonState()
+
+	function amtField:onUnFocus()
+		return confirmTransferButtonState()
+	end
+
+	function toField:onUnFocus()
+		return confirmTransferButtonState()
+	end
 
 	return transactionPanel
 end
 
+local function makeActiveSidebarButton(btn)
+	for _,v in pairs(sidebarButtons) do
+		v.style:setField("textColour", sheets.colour.black)
+	end
+
+	btn.style:setField("textColour", sheets.colour.white)
+end
+
 local function setInfoContainer(container, callback)
 	screen:removeChild(infoContainer)
-	infoContainer = screen + container
+	infoContainer = walletContainer + container
 
 	if callback then
 		return callback()
@@ -177,16 +275,36 @@ end
 
 function sidebarButtons.overview:onClick(btn)
 	if btn == 1 then
+		makeActiveSidebarButton(sidebarButtons.overview)
 		return setInfoContainer(createOverviewPanel())
 	end
 end
 
 function sidebarButtons.transactions:onClick(btn)
 	if btn == 1 then
+		makeActiveSidebarButton(sidebarButtons.transactions)
 		return setInfoContainer(createTransactionPanel())
 	end
 end
 
-setInfoContainer(createOverviewPanel())
+function startButton:onClick(btn)
+	if btn == 1 and startButton.validAddress then
+		local addr, pkey = krist.login(privatekeyBox.text)
+		addressInUse = addr
+		privatekey = pkey
+
+		if addr == nil then
+			startButton.style:setField("colour", sheets.colour.red)
+			return
+		end
+
+		introContainer:animateX(-screen.width)
+		walletContainer:animateX(0)
+
+		setInfoContainer(createOverviewPanel())
+	end
+end
+
+makeActiveSidebarButton(sidebarButtons.overview)
 
 app:run()
